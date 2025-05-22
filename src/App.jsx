b91abb3726dc927b853fff5c10f32b18b2448ebd
StevexG7 <nitsongarcia@gmail.com>
2025-05-21 22:11:34 -0500

    Agregado de paquetes y dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./Pages/Home"
 import { NoEncontrado } from "./Pages/NoEncontrado"
 
+
 function App() {
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path='*' element={<NoEncontrado />} /> 
+          <Route path='*' element={<NoEncontrado />} />
         </Routes>
       </BrowserRouter>
     </>
